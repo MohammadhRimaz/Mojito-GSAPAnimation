04add0eacb7bfe7d8fb93031453dd8f5199b5dfd
MohammadhRimaz <mohammadhrimaz@gmail.com>
2025-07-16 12:23:39 +0530

    Learned GSAP scroll trigger animations in GsapScrollTrigger component

--- src/pages/GsapScrollTrigger.jsx
+++ src/pages/GsapScrollTrigger.jsx
@@ -1,5 +1,41 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import { useRef } from "react";
+
+gsap.registerPlugin(ScrollTrigger);
+
 const GsapScrollTrigger = () => {
   // TODO: Implement the gsap scroll trigger
+  const scrollRef = useRef();
+
+  useGSAP(
+    () => {
+      const boxes = gsap.utils.toArray(scrollRef.current.children);
+
+      boxes.forEach((box) => {
+        gsap.to(box, {
+          x: 150 * (boxes.indexOf(box) + 5),
+          // First box: x = 150 * (0 + 5) = 750
+          // Second box: x = 150 * (1 + 5) = 900
+          rotation: 360,
+          borderRadius: "100%",
+          scale: 1.5,
+          scrollTrigger: {
+            trigger: box,
+            start: "bottom bottom",
+            // The first "bottom" refers to the bottom edge of the trigger element (the box).
+            // The second "bottom" refers to the bottom edge of the viewport (browser window).
+            // So, the animation starts when the bottom of the box touches the bottom of the viewport as you scroll.
+            end: "top 20%", // Animation ends at 20% viewpoint of the box
+            scrub: true, // Animation smooth
+          },
+          ease: "power1.inOut",
+        });
+      });
+    },
+    { scope: scrollRef }
+  );
 
   return (
     <main>
@@ -51,7 +87,7 @@ const GsapScrollTrigger = () => {
         </svg>
       </div>
 
-      <div className="mt-20 w-full h-screen">
+      <div className="mt-20 w-full h-screen" ref={scrollRef}>
         <div
           id="scroll-pink"
           className="scroll-box w-20 h-20 rounded-lg bg-pink-500"
