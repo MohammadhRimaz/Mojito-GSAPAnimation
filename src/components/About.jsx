f4dc5a923fa3be80be07e29857d5b691e7163f8c
MohammadhRimaz <mohammadhrimaz@gmail.com>
2025-07-25 21:01:51 +0530

    feat: add Menu component with GSAP animations and integrate into App; update About component to include paragraph in SplitText animation; adjust CSS positioning for left leaf

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import { SplitText } from "gsap/all";
 
 const About = () => {
   useGSAP(() => {
-    const titleSplit = SplitText.create("#about h2", {
+    const titleSplit = SplitText.create("#about h2, p", {
       type: "words",
     });
 
